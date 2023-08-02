981760c0c2d9d36796c9c1614d5a0856e695a19c
Márcio Fernando Maia <59803321+marciomaia2020@users.noreply.github.com>
2023-08-02 19:30:51 -0300

    Update files

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        JOptionPane.showMessageDialog(null, "Ola");
+    
+    }
+}
+/*Instale a extensão, Reinicie o VScode
+CRTL + SHIF + P, digite Java, Procure por Create New Java Project
+Apnte a pasta onde será gravados os projetos
+Crie um projeto vazio e de um nome para ele ex: Olá mundo
+Ele criará o projeto com as pastas relacionadas
+Pronto pra rodar os programas*/
